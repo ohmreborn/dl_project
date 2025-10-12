178aec643aadcef3c8cf38be15e3d74a4c0400b5
ohm <krittiwitkampradam@gmail.com>
2025-10-12 08:52:13 +0000

    add build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-
-source ./myenv/bin/activate
+# source ./myenv/bin/activate
 prefix_path=$(python -c "import torch, os; print(os.path.join(os.path.dirname(torch.__file__), 'share/cmake/Torch'))")
 cmake -B build . -DCMAKE_PREFIX_PATH=$prefix_path
 cmake --build build
